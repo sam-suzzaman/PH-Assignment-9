c1e09756d102d3879b2eb8b25dc6d64654ff8505
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-04-04 15:00:25 +0600

    showing reviews in Reviews page

--- src/components/pages/ReviewsPage/ReviewsPage.js
+++ src/components/pages/ReviewsPage/ReviewsPage.js
@@ -1,8 +1,15 @@
 import React from "react";
+import CustomerReviews from "../../CustomerReviews/CustomerReviews";
+import useReviewsData from "../../utilities/useReviewsData";
 import "./ReviewsPage.css";
 
 const ReviewsPage = () => {
-    return <div>from reviews page</div>;
+    const { reviews } = useReviewsData();
+    return (
+        <section className="review-page-wrapper">
+            <CustomerReviews reviewsData={reviews} />
+        </section>
+    );
 };
 
 export default ReviewsPage;
